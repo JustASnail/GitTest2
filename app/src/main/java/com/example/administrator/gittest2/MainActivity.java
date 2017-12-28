366afa9ec90e8aa91e4f7d29acc24a9250066248
ZhangSan <511137534@qq.com>
2017-12-28 11:24:36 +0800

    我刚才创建了   1111远程

--- app/src/main/java/com/example/administrator/gittest2/MainActivity.java
+++ app/src/main/java/com/example/administrator/gittest2/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("我刚才创建了几个分之");
         System.out.println("我刚才创建了远程分之");
         System.out.println("我刚才创建了远程分之");
+        System.out.println("我刚才创建了远程分之");
     }
 }
