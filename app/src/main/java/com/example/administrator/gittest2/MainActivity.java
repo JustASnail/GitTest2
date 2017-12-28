92a2d1b3001ae60da9eb651a1b37ed7f3509bb1e
ZhangSan <511137534@qq.com>
2017-12-28 10:41:50 +0800

    第一次修改内容提交到远程仓库

--- app/src/main/java/com/example/administrator/gittest2/MainActivity.java
+++ app/src/main/java/com/example/administrator/gittest2/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("第一次修改项目内容提交到仓库");
     }
 }
