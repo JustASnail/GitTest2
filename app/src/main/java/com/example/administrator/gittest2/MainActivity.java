ce59bf8763d33267a9c73d304766a1c04fa000cf
ZhangSan <511137534@qq.com>
2017-12-28 10:55:25 +0800

    我刚才创建了几个分之

--- app/src/main/java/com/example/administrator/gittest2/MainActivity.java
+++ app/src/main/java/com/example/administrator/gittest2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("第一次修改项目内容提交到仓库");
+        System.out.println("我刚才创建了几个分之");
     }
 }
